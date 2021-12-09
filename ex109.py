0673f0acf159a8cc4b3a3ed75cbba16403bd1237
TREMOUILLEThomas <90332661+TREMOUILLEThomas@users.noreply.github.com>
2021-12-09 15:21:47 +0000

    Add files via upload

---
+++ ex109.py
@@ -0,0 +1,7 @@
+for i in range(1000):
+    for j in range(1000):
+        for k in range(1000):
+            for l in range(1000):
+                print ("Mille sabords!")
+                
+#le programme affcihe 10 puissance 4 valeurs#
